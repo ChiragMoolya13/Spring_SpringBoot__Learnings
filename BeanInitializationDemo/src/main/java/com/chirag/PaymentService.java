5ffb769a8af698fef0755e0af2a2504f8e87f584
Chirag Moolya <chiragmoolya13@gmail.com>
2026-06-23 12:30:44 +0530

    Bean Scope, eager initialization and lazy initialization

---
+++ BeanInitializationDemo/src/main/java/com/chirag/PaymentService.java
@@ -0,0 +1,21 @@
+package com.chirag;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class PaymentService {
+
+    OrderService orderService;
+
+    public PaymentService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public void pay() {
+        System.out.println("Payment Successful");
+        orderService.getOrderDetails();
+    }
+}
---
+++ BeanScopeDemo/src/main/java/com/chirag/OrderService.java
@@ -0,0 +1,15 @@
+package com.chirag;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+    public OrderService() {
+        System.out.println("OrderService created");
+    }
+
+    public void placeOrder(){
+        System.out.println("Order Placed");
+    }
+}
