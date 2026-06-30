d431c0d68d326711e91f70ff5d23481a87ac36ef
Chirag Moolya <chiragmoolya13@gmail.com>
2026-06-30 21:05:11 +0530

    Spring JPA Methods

--- crudSpringBootDemo/src/main/java/com/chirag/crudSpringBootDemo/controller/StudentController.java
+++ crudSpringBootDemo/src/main/java/com/chirag/crudSpringBootDemo/controller/StudentController.java
@@ -6,10 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -22,17 +21,50 @@ public class StudentController {
     }
     @PostMapping("/create")
     public ResponseEntity<Student> createStudent(@RequestBody Student student){
-        System.out.println("Inside Student Controller");
         Student createdStudent = studentService.createStudent(student);
-        System.out.println("Exiting Student Controller");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(createdStudent);
     }
 
     //read student
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id){
+        Student studentResp = studentService.getStudent(id);
+        if(studentResp == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(studentResp);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Student>> getAllStudent(){
+        List<Student> studentList = studentService.getAllStudent();
+        if(studentList.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(studentList);
+    }
 
     //update student
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentReq){
+        Student studentResp = studentService.updateStudent(id, studentReq);
+        if(studentResp == null){
+            return ResponseEntity.notFound().build();
+        }else{
+            return ResponseEntity.ok(studentResp);
+        }
+    }
 
     //delete student
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id){
+        Boolean isDeleted = studentService.deleteStudent(id);
+        if(!isDeleted){
+            return ResponseEntity.notFound().build();
+        }else{
+            return ResponseEntity.ok("Record deleted");
+        }
+    }
 }
--- crudSpringBootDemo/src/main/java/com/chirag/crudSpringBootDemo/repository/StudentRepository.java
+++ crudSpringBootDemo/src/main/java/com/chirag/crudSpringBootDemo/repository/StudentRepository.java
@@ -1,21 +1,11 @@
 package com.chirag.crudSpringBootDemo.repository;
 
 import com.chirag.crudSpringBootDemo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
-public class StudentRepository {
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    public Student saveStudent(Student studentReq){
-        System.out.println("Inside Student Repository");
-        System.out.println("Exiting Student Repository");
-        Student s1 = new Student();
-        s1.setName("Chirag");
-        s1.setAge(30);
-        s1.setEmail("chirag@gmail.com");
-        s1.setRollNo(101);
-        s1.setSubject("Spring Framework");
-
-        return s1;
-    }
 }
