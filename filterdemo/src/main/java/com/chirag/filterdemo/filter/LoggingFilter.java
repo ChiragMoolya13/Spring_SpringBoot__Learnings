f99e985594e1fc80c7b82daf684dee2f89b54a01
Chirag Moolya <chiragmoolya13@gmail.com>
2026-07-12 20:29:48 +0530

    FilterChain, Logging, Auth & Request Flow

---
+++ filterdemo/src/main/java/com/chirag/filterdemo/filter/LoggingFilter.java
@@ -0,0 +1,49 @@
+package com.chirag.filterdemo.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(2)
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        long startTime = System.currentTimeMillis();
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        String requestId = UUID.randomUUID().toString();
+
+        httpResponse.setHeader("X-Request-ID", requestId);
+
+        // Request Log
+        System.out.println("Incoming Request : " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        // Response status Log
+        System.out.println("Response Status : " + httpResponse.getStatus());
+
+        System.out.println("API Response Time : " + duration);
+        }
+
+
+
+    }
+}
